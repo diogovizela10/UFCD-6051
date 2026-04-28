08f083d81c3044e3d56a8d3ef1235d893e3e6a45
diogovizela10 <diogo10vizela@gmail.com>
2026-04-28 20:07:16 +0100

    lista_mesas_e_pc_final

--- sessao_24_04_2026/listas_mesas_e_pc.py
+++ sessao_24_04_2026/listas_mesas_e_pc.py
@@ -55,14 +55,14 @@ while True:
                  "ram": "16GB", 
                  "armazenamento": "512GB SSD"},
    
-   Computador5= {"marca": "Asus", 
+   computador5= {"marca": "Asus", 
                  "modelo": "ZenBook 14",        
                 "processador": "Intel Core i7",
                 "ram": "16GB",
                 "armazenamento": "512GB SSD"},  
    
    lista_das_mesas = [mesa1,mesa2,mesa1,mesa2,mesa1],
-   lista_dos_computadores = [computador1, computador2, computador3, computador4, Computador5],
+   lista_dos_computadores = [computador1, computador2, computador3, computador4, computador5],
    lista_dos_formandos = ["diogo", "joão", "rui machado","ricardo", "José"],
 
 Print["Lista_das_mesas","Listas_dos_computadores", "Lista_dos_formandos" ]
