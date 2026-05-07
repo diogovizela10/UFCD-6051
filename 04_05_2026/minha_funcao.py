9213959b7e7a4fe020b24ce158bb6f2f33bbd3da
diogovizela10 <diogo10vizela@gmail.com>
2026-05-07 14:43:26 +0100

    Delete 04_05_2026 directory

--- 04_05_2026/minha_funcao.py
+++
@@ -1,2 +0,0 @@
-def xnor(entrada1,entrada2):
-        return not (entrada1 and not entrada2) or (not entrada1 and entrada2)
--- 04_05_2026/principal2.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-root = tkinter.tk()
-
-btn= tkinter.button(text="Pressiona")
-
-btn.pack()
-
-root.mainloop()
